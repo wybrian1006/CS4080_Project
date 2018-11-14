2199dc0d3567538baf4509ac1292ecde39f16699
Brian Cho <wonyoungcho@cpp.edu>
2018-11-14 01:45:05 -0800

    Update Main.R

--- Avocado/Main.R
+++ Avocado/Main.R
@@ -32,50 +32,91 @@ while(TRUE){
     displayRegion()
     num <- readline(prompt = "Select a region (1~54): ")
     num = as.numeric(num)
-    x = ((num-1)*52 + 1)
-    y = num*52
-    hist(myvalues$AveragePrice[x:y], ylim = c(0,2), main = "Average price of selected region", 
-         xlab = "Price of Avocado", col = "blue", breaks = 10, probability = F)
-    cat('\n')
+    if ((num >= 1) && (num <=54)) {
+      x = ((num-1)*52 + 1)
+      y = num*52
+      hist(myvalues$AveragePrice[x:y], main = "Average price of selected region", 
+          xlab = "Price of Avocado", col = "blue", probability = F)
+      cat('\n')
+    } else {
+      cat('You entered invalid number... Please try again!\n\n')
+    }
   } else if(choice == 2){
-    displayYear()
+    displayRegion()
     num <- readline(prompt = "Select a region (1~54): ")
     num = as.numeric(num)
-    x = ((num-1)*52 + 1)
-    y = num*52
-    hist(myvalues$AveragePrice[x:y], ylim = c(0,15), main = "Average price of selected region", 
-         xlab = "Price of Avocado", col = "blue", breaks = 10, probability = T)
-    cat('\n')
+    if ((num >= 1) && (num <=54)) {
+      x = ((num-1)*52 + 1)
+      y = num*52
+      hist(myvalues$AveragePrice[x:y], main = "Average price of selected region", 
+           xlab = "Price of Avocado", col = "blue", probability = T)
+      cat('\n')
+    } else {
+      cat('You entered invalid number... Please try again!\n\n')
+    }
   } else if(choice == 3){
     displayYear()
     num <- readline(prompt = "Select a year (xxxx): ")
     num = as.numeric(num)
-    year1 <- (subset(myvalues, year == num))$AveragePrice
-    hist(year1, main = "Average price of selected region", 
-         xlab = "Price of Avocado", col = "blue", probability = F)
-    cat('\n')
+    if ((num >= 2015) && (num <= 2018)) {
+      year1 <- (subset(myvalues, year == num))$AveragePrice
+      hist(year1, main = "Average price of selected region", 
+           xlab = "Price of Avocado", col = "blue", probability = F)
+      cat('\n')
+    } else {
+      cat('You entered invalid number... Please try again!\n\n')
+    }
   } else if(choice == 4){
     displayYear()
     num <- readline(prompt = "Select a year (xxxx): ")
     num = as.numeric(num)
-    year1 <- (subset(myvalues, year == num))$AveragePrice
-    hist(year1, main = "Average price of selected region", 
-         xlab = "Price of Avocado", col = "blue", probability = T)
-    cat('\n')
+    if ((num >= 2015) && (num <= 2018)) {
+      year1 <- (subset(myvalues, year == num))$AveragePrice
+      hist(year1, main = "Average price of selected region", 
+           xlab = "Price of Avocado", col = "blue", probability = T)
+      cat('\n')
+    } else {
+      cat('You entered invalid number... Please try again!\n\n')
+    }
   } else if(choice == 5){
     
   } else if(choice == 6){
     
   } else if(choice == 7){
-    reg1in <- readline(prompt = "Select a region(1 to 54): ")
+    num <- readline(prompt = "Select a region(1 to 54): ")
+    num = as.numeric(num)
+    x = ((num-1)*52 + 1)
+    y = num*52
     
   } else if(choice == 8){
-    reg1in <- readline(prompt = "Select a region(1 to 54): ")
+    num <- readline(prompt = "Select a year (xxxx): ")
     
   } else if(choice == 9){
-    reg1in <- readline(prompt = "Select a region(1 to 54): ")
-    
+    num <- readline(prompt = "Select a region(1 to 54): ")
+    num = as.numeric(num)
+    if ((num >= 1) && (num <= 54)) {
+      x = ((num-1)*52 + 1)
+      y = num*52
+      index1 = which.min(myvalues$AveragePrice[x:y])
+      lowest = myvalues$AveragePrice[index1]
+      index2 = which.max(myvalues$AveragePrice[x:y])
+      highest = myvalues$AveragePrice[index2]
+      cat('The lowest price is', lowest, ',and the highest price is', highest, '\n\n')
+    } else {
+      cat('You entered invalid number... Please try again!\n\n')
+    }
   } else if(choice == 10){
+    num <- readline(prompt = "Select a year (xxxx): ")
+    num = as.numeric(num)
+    if ((num >= 2015) && (num <= 2018)) {
+      year1 <- (subset(myvalues, year == num))$AveragePrice
+      lowest = year1[which.min(year1)]
+      highest = year1[which.max(year1)]
+      cat('The lowest price is', lowest, ',and the highest price is', highest, '\n\n')
+    } else {
+      cat('You entered invalid number... Please try again!\n\n')
+    }
+  } else if(choice == 11){
     break
   }
 }
