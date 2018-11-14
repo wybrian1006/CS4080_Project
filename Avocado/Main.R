8494b7cd90edd110007e29f91e7d97d70fb83e7c
Brian Cho <wonyoungcho@cpp.edu>
2018-11-14 01:25:40 -0800

    Update Main.R
    Still working on it. Will be finished soon.

--- Avocado/Main.R
+++ Avocado/Main.R
@@ -1,18 +1,26 @@
 ##########################################################################
 # CS4080_Group_Project
-# Last Date Modified as: 10/31/2018
+# Last Date Modified as: 11/14/2018
 #
-# Draw Histogram, Box plot, etc..
-# To calculate mean value, variant, standard deviation, and average.
+# Prompt user to choose menu items, and outputs the graph and datas.
+# Draw histograms(frequency & density), calculate lowest & highest price.
 ##########################################################################
 
 # Menu runs
-menuItems <- c("Make region by region histogram (All year)",
-"Make region by region histogram (specific year)",
-"Make year by year histogram (All year)",
-"Make region by region box plot (All year)",
-"Make region by region box plot (specific year)",
-"Make year by year box plot (All year)",
+menuItems <- c("Make frequency histogram of a region",
+"Make density histogram of a region",
+
+"Make frequency histogram of a year",
+"Make density histogram of a year",
+
+"Make box plot of a region",
+"Make box plot of a year",
+
+"Calculate the mean average price of a region",
+"Calculate the mean average price of a year",
+
+"Get the lowest & highest price of a region",
+"Get the lowest & highest price of a year",
 "Exit")
 
 name <- ""
@@ -22,19 +30,52 @@ while(TRUE){
   
   if(choice == 1){
     displayRegion()
-    reg <- readline(prompt = "Which region would you like to select?(1 to 54): ")
-    makeHist(reg)
+    num <- readline(prompt = "Select a region (1~54): ")
+    num = as.numeric(num)
+    x = ((num-1)*52 + 1)
+    y = num*52
+    hist(myvalues$AveragePrice[x:y], ylim = c(0,2), main = "Average price of selected region", 
+         xlab = "Price of Avocado", col = "blue", breaks = 10, probability = F)
+    cat('\n')
   } else if(choice == 2){
-    
+    displayYear()
+    num <- readline(prompt = "Select a region (1~54): ")
+    num = as.numeric(num)
+    x = ((num-1)*52 + 1)
+    y = num*52
+    hist(myvalues$AveragePrice[x:y], ylim = c(0,15), main = "Average price of selected region", 
+         xlab = "Price of Avocado", col = "blue", breaks = 10, probability = T)
+    cat('\n')
   } else if(choice == 3){
-    
+    displayYear()
+    num <- readline(prompt = "Select a year (xxxx): ")
+    num = as.numeric(num)
+    year1 <- (subset(myvalues, year == num))$AveragePrice
+    hist(year1, main = "Average price of selected region", 
+         xlab = "Price of Avocado", col = "blue", probability = F)
+    cat('\n')
   } else if(choice == 4){
-    
+    displayYear()
+    num <- readline(prompt = "Select a year (xxxx): ")
+    num = as.numeric(num)
+    year1 <- (subset(myvalues, year == num))$AveragePrice
+    hist(year1, main = "Average price of selected region", 
+         xlab = "Price of Avocado", col = "blue", probability = T)
+    cat('\n')
   } else if(choice == 5){
     
   } else if(choice == 6){
     
   } else if(choice == 7){
+    reg1in <- readline(prompt = "Select a region(1 to 54): ")
+    
+  } else if(choice == 8){
+    reg1in <- readline(prompt = "Select a region(1 to 54): ")
+    
+  } else if(choice == 9){
+    reg1in <- readline(prompt = "Select a region(1 to 54): ")
+    
+  } else if(choice == 10){
     break
   }
 }
